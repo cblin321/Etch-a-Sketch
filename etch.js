d79d652955b5e388e31ce53e209bb76c616758e3
Chris Lin <chrislin90210@gmail.com>
2023-08-26 15:54:08 -0500

    made progress on darkening but seems pretty annoying to implement

--- etch.js
+++ etch.js
@@ -14,13 +14,15 @@ function initGrid(n, rainbow = false) {
             container.appendChild(div);
             div.addEventListener(("mouseenter"), (e) => {
                 if (!rainbow)
-                    div.style.backgroundColor = "blue";
+                    div.style.backgroundColor = "rgb(0, 0, 255)";
                 else {
                     console.log("rainbow on");
                 }
+                let color = getComputedStyle(div).backgroundColor
+                console.log(color.slice(4, color.length - 1).split(","));
             });
-            div.style.height = `calc(700px / ${n})`;
-            div.style.width = `calc(700px / ${n})`;
+            div.style.height = `calc(${container.offsetHeight}px / ${n})`;
+            div.style.width = `calc(${container.offsetWidth}px / ${n})`;
         }
     }
     container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
@@ -48,4 +50,49 @@ function rainbowMode(pixels, rainbowState) {
             }
         }
     }
+}
+
+function resize(pixels) {
+    if (window.innerHeight < 350 || window.innerWidth < 350) {
+        for (let i = 0; i < pixels.length; i++) {
+            for (let j = 0; j < pixels[i].length; j++) {
+                div = pixels[i][j];
+                div.style.height = `calc(${container.offsetHeight}px / ${n})`;
+                div.style.width = `calc(${container.offsetWidth}px / ${n})`;
+            }
+        }
+    }
+}
+
+function darken(pixel) {
+    const RGBToHSL = (r, g, b) => {
+        r /= 255;
+        g /= 255;
+        b /= 255;
+        const l = Math.max(r, g, b);
+        const s = l - Math.min(r, g, b);
+        const h = s
+          ? l === r
+            ? (g - b) / s
+            : l === g
+            ? 2 + (b - r) / s
+            : 4 + (r - g) / s
+          : 0;
+        return [
+          60 * h < 0 ? 60 * h + 360 : 60 * h,
+          100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
+          (100 * (2 * l - s)) / 2,
+        ];
+      };
+      let color = getComputedStyle(pixel).backgroundColor;
+      if (color.slice(0, 3) == "rgb") {
+        color = color.slice(4, color.length - 1).split(",");
+        for (let i = 0; i < color.length; i++) {
+          color[i] = color[i].trim();
+        }
+        let hsl = RGBToHSL(color[0], color[1], color[2]);
+        pixel.style.backgroundColor = `hsl(${hsl[0], hsl[1], hsl[2] - (hsl)})`; //TODO check return value
+      }
+
+
 }
